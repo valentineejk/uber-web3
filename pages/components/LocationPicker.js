1f82677b3d6c0f3ebc3610bd4daec5a028bb883d
miskayapp <100726776+miskayapp@users.noreply.github.com>
2022-03-07 09:07:47 +0100

    connecting map feature

---
+++ pages/components/LocationPicker.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const style={
+    wrapper: `pt-2`
+}
+
+const LocationPicker=() => {
+    return (
+        <div style={style.wrapper}>LocationPicker</div>
+    )
+}
+
+export default LocationPicker
--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import avater from '/img/Avater.PNG'
+import {BsPerson} from 'react-icons/bs'
 
 const style={
     wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
@@ -11,7 +12,7 @@ const style={
     userImageContainer: `mr-2`,
     userImage: ` h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
     loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`,
-    loginTexr: ` ml-2 `,
+    loginText: ` ml-2 `,
 
 }
 
@@ -38,7 +39,10 @@ const Navbar=() => {
                     <div>{currentUser.slice(0,6)}...{currentUser.slice(39)}</div>
 
                 ):(
-                    <div>Please Log In</div>
+                    <div className={style.loginButton}>
+                        <BsPerson />
+                        <span className={style.loginText}>Log in</span>
+                    </div>
 
                 )}
 
